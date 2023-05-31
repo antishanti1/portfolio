436cf61685d4c5636bb8d49accb641f167246565
antishanti1 <shutkova2603@gmail.com>
2023-05-31 19:51:03 -0400

    fixing 2.0

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -10,7 +10,7 @@ export default function Main() {
         <section className='main'>
             <div className='main__top'>
             <p className='main__name'>
-                Hi! My name is Alina Shutkova.
+                Hi! My name is Alina.
             </p>
             <p className='main__me'>
             I am a <span className='main__me--bold'>Full Stack Web Developer</span> based in Miami. With a background in car sales and financing, I bring a unique perspective to my work, combining my technical skills with a deep understanding of the industry. Explore my projects and experience to see how I can help bring your digital ideas to life.
